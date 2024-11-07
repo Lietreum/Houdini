d00e2978e330ea56ce13a04ea9775d2d12b7138f
Lietreum <mirsieolien@gmail.com>
2024-11-07 23:59:44 +0700

    kapan kapan lagi biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
-import million from 'million/compiler';
-import { visualizer } from 'rollup-plugin-visualizer';
-import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -32,13 +29,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    million.vite({ auto: true }),
     react(),
     checker({
       typescript: true,
     }),
     tsconfigPaths(),
-    visualizer({ template: 'sunburst' }) as unknown as PluginOption,
     // VitePWA(pwaOptions),
   ],
   server: {
