3246f1f0849fd93de96704af81b39345759e69fe
Lietreum <mirsieolien@gmail.com>
2024-11-07 23:08:28 +0700

    f

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 // fonts
 import '@fontsource-variable/plus-jakarta-sans';
 
-import { App } from '@/app';
+import { App } from '../src/app';
 
 import "../src/css/index.css";
 
